164722d340d94bfe869518e04b827580f10a5dbc
wonwoo-kim <54891281+wonwoo-kim@users.noreply.github.com>
2020-06-19 08:36:38 +0900

    Add files via upload

---
+++ makefrozen1.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow.python.tools import freeze_graph
+
+freeze_graph.freeze_graph(input_graph='./nonfrozenpbtxt.pbtxt',
+                          input_saver="",
+                          input_binary=False,
+                          input_checkpoint='./final.ckpt',
+                          output_node_names='add_37',
+                          restore_op_name="",
+                          filename_tensor_name="",
+                          output_graph='./make_frozen1.pb',
+                          clear_devices=False, initializer_nodes="")
